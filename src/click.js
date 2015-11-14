9c193e8510c203c5897a6459a7c274ba8597f7cc
lispincsclothing <scullingnull+1@gmail.com>
2015-11-14 12:16:36 -0800

    Adding styles (rather than using css)

--- src/click.js
+++ src/click.js
@@ -2,6 +2,20 @@
 // State, on the other hand, is internal to document (dynamic data over time)
 // Don't ever set state directly, e.g this.state.clicks=1.  Circumvents this.setState - use that!
 
+
+var styles={
+  clickText: {
+    textDecoration:'underline',
+    color:'green',
+    fontSize:'48px'
+  },
+  countText:{
+    fontSize:'60px',
+    color: 'acqumarine',
+    fontWeight:'600'
+  }
+};
+
 var ClickCounter = React.createClass({
   getInitialState: function() {
     return {clicks: 0};
@@ -14,8 +28,8 @@ var ClickCounter = React.createClass({
     return (
       <div>
       <button onClick={this.handleBtnClick} className="btn btn-primary">
-      { this.props.text }
-      <span className="badge">
+      <span style={styles.clickText}>{ this.props.text }</span>
+      <span className="badge" style={styles.countText}>
         { this.state.clicks }
       </span>
       </button>
@@ -24,7 +38,6 @@ var ClickCounter = React.createClass({
     );
   }
 });
-
 
 var ClickedNums = React.createClass({
   render: function(){
